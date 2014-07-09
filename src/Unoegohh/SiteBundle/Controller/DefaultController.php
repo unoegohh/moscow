8ce79bcbce6e4557423b1ccefcd9213dc85fbaf2
yurikarmakov <unoegohh@gmail.com>
2014-07-09 13:44:07 +0400

    wefwef

--- src/Unoegohh/SiteBundle/Controller/DefaultController.php
+++ src/Unoegohh/SiteBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository("UnoegohhEntitiesBundle:Post");
-        $posts = $repo->findBy(array(), array('id' => 'DESC'));
+        $posts = $repo->findBy(array(), array('date' => 'DESC'));
         return $this->render('UnoegohhSiteBundle:Default:index.html.twig', array('posts' => $posts, "_locale" =>$_locale ));
     }
     public function redirectAction()
