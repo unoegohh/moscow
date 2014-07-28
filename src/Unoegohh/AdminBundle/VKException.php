4e597696e871ab5741f4c8eac8ff629699d32f82
yurikarmakov <unoegohh@gmail.com>
2014-07-29 00:47:19 +0400

    wefwef

---
+++ src/Unoegohh/AdminBundle/VKException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Unoegohh\AdminBundle;
+/**
+ * The PHP class for vk.com API and to support OAuth.
+ * @author Vlad Pronsky <vladkens@yandex.ru>
+ * @license https://raw.github.com/vladkens/VK/master/LICENSE MIT
+ * @version 0.1.6
+ */
+class VKException extends \Exception {  }
